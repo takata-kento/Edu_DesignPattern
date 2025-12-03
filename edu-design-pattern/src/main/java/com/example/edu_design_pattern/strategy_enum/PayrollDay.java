dc18b39878118acf768525907abece4ee1a0d622
takata-kento <tkt1996knt@gmail.com>
2025-12-03 23:33:58 +0900

    feat: strategy enum

---
+++ edu-design-pattern/src/main/java/com/example/edu_design_pattern/strategy_enum/PayrollDay.java
@@ -0,0 +1,44 @@
+package com.example.edu_design_pattern.strategy_enum;
+
+public enum PayrollDay {
+    MONDAY(PayType.WEEKDAY),
+    TUESDAY(PayType.WEEKDAY),
+    WEDNESDAY(PayType.WEEKDAY),
+    THURSDAY(PayType.WEEKDAY),
+    FRIDAY(PayType.WEEKDAY),
+    SATURDAY(PayType.WEEKDAY),
+    SUNDAY(PayType.WEEKDAY);
+
+    private final PayType payType;
+
+    PayrollDay(PayType payType) {
+        this.payType = payType;
+    }
+
+    public int pay(int minutesWorked, int payRate) {
+        return payType.pay(minutesWorked, payRate);
+    }
+
+    private enum PayType {
+        WEEKDAY {
+            int overtimePay(int minsWorked, int payRate) {
+                return minsWorked <= MINS_PER_SHIFT ? 0 :
+                    (minsWorked - MINS_PER_SHIFT) * payRate / 2;
+            }
+        },
+        WEEKEND {
+            int overtimePay(int minsWorked, int payRate) {
+                return minsWorked * payRate / 2;
+            }
+        };
+
+        private static final int MINS_PER_SHIFT = 8 * 60;
+
+        abstract int overtimePay(int mins, int payRate);
+
+        int pay(int minsWorked, int payRate) {
+            int basePay = minsWorked * payRate;
+            return basePay * overtimePay(minsWorked, payRate);
+        }
+    }
+}
